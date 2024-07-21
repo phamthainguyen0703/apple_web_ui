d8cab3bea964976e67fdce54c44046d84db3d51e
Pham Thai Nguyen <phamthainguyen0703@gmail.com>
2024-07-21 20:13:32 +0700

    update

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -13,7 +13,7 @@ import { animateWithGsap, animateWithGsapTimeline } from "../utils/animations";
 const Model = () => {
   const [size, setSize] = useState("small");
   const [model, setModel] = useState({
-    title: "iPhone 15 Pro in Natural Titanium",
+    title: "iPhone 15 Pro Titan Tự Nhiên",
     color: ["#8F8A81", "#FFE7B9", "#6F6C64"],
     img: yellowImg,
   });
